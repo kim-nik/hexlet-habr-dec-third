d68622ec5e0a32af300e780f7a8cbcceb4846097
N1kk1337 <nikillerok@gmail.com>
2024-12-15 20:54:58 +0200

    feat: add search

--- src/app/components/Board.tsx
+++ src/app/components/Board.tsx
@@ -4,8 +4,8 @@ import React, { useEffect, useState } from "react";
 import { monitorForElements } from "@atlaskit/pragmatic-drag-and-drop/element/adapter";
 import Column from "./Column";
 import {
-  initializeDocuments,
   updateDocumentStatus,
+  initializeDocuments,
 } from "../../lib/features/docs/documentsSlice";
 import { RootState } from "../../lib/store";
 import { useAppDispatch, useAppSelector } from "@/lib/hooks";
@@ -14,7 +14,7 @@ const LOCAL_STORAGE_KEY = "kanban_documents";
 
 const Board: React.FC = () => {
   const [isInitialized, setIsInitialized] = useState(false);
-
+  const [filterText, setFilterText] = useState("");
   const documents = useAppSelector(
     (state: RootState) => state.documents.documents
   );
@@ -56,18 +56,35 @@ const Board: React.FC = () => {
       },
     });
   }, [dispatch]);
+
   if (!isInitialized) {
     return <div>Loading...</div>; // Show a loading state until documents are initialized
   }
+
+  const filteredDocuments = documents.filter((doc) =>
+    doc.title.toLowerCase().includes(filterText.toLowerCase())
+  );
+
   return (
-    <div className="flex gap-4">
-      {["in-progress", "under-review", "completed"].map((status) => (
-        <Column
-          key={status}
-          status={status}
-          documents={documents.filter((doc) => doc.status === status)}
+    <div>
+      <div className="mb-4">
+        <input
+          type="text"
+          value={filterText}
+          onChange={(e) => setFilterText(e.target.value)}
+          placeholder="Поиск по заголовку"
+          className="border p-2 w-full text-black"
         />
-      ))}
+      </div>
+      <div className="flex gap-4">
+        {["in-progress", "under-review", "completed"].map((status) => (
+          <Column
+            key={status}
+            status={status}
+            documents={filteredDocuments.filter((doc) => doc.status === status)}
+          />
+        ))}
+      </div>
     </div>
   );
 };
